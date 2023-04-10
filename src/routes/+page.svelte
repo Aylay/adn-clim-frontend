475a6cd14f2227965cd2cb9830c4988730a93df6
Aylay <luk.tattali@gmail.com>
2023-04-10 15:05:42 +0200

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import "../app.css";
+  
+  import Logo from "$lib/components/svg/Logo.svelte";
+
+  const travaux = ['Plomberie', 'Climatisation', 'Chauffage', 'Piscine']
+</script>
+
+<div class="w-full h-screen flex justify-center items-center px-[5%]">
+  <div class="flex flex-col max-w-[65rem]">
+    <Logo newClass="w-full h-auto" />
+    <div class="lg:flex justify-between max-lg:grid max-lg:grid-cols-2 max-lg:gap-8 mt-8">
+      {#each travaux as travail}
+        <p class="text-blue uppercase text-4xl max-lg:odd:text-right max-lg:even:text-left">{travail}</p>
+      {/each}
+    </div>
+    <div class="my-32 flex lg:justify-between max-lg:flex-col gap-4">
+      <a class="underline hover:no-underline text-blue text-3xl" href="tel:+33640366615">
+        06 40 36 66 15
+      </a>
+      <a class="underline hover:no-underline text-blue text-3xl" href="mailto:adnclim@gmail.com">
+        adnclim@gmail.com
+      </a>
+    </div>
+    <p class="text-center text-5xl">Site en construction</p>
+  </div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,68 +5,13 @@ module.exports = {
 		colors: {
 			white: '#FFF',
 			black: '#000',
-			jagger: '#380A51',
-			rock: '#180325',
-			jaguar: '#0B0211',
-			bright: '#00FFDA',
-			seance: '#8013BD'
+			blue: '#0E18FF',
+			spray: '#72EDCA'
 		},
 		fontFamily: {
-			base: 'Montserrat, Arial, sans-serif',
-			highlight: 'Cookie, Arial, sans-serif'
+			base: "'Fira Sans', Arial, sans-serif"
 		},
-		fontSize: {
-			1: [
-				'10rem',
-				{
-					lineHeight: '12rem',
-					fontWeight: '700'
-				}
-			],
-			2: [
-				'7.5rem',
-				{
-					lineHeight: '9rem',
-					fontWeight: '600'
-				}
-			],
-			3: [
-				'4rem',
-				{
-					lineHeight: '4.8rem',
-					fontWeight: '600'
-				}
-			],
-			4: ['3rem', '3rem'],
-			5: [
-				'2.3rem',
-				{
-					lineHeight: '2.76rem',
-					fontWeight: '600'
-				}
-			],
-			6: ['1.6rem', '2.4rem']
-		},
-		extend: {
-			animation: {
-				line: 'line 1s ease-in-out',
-				title: 'title 2s linear 1s 1 forwards',
-			},
-			keyframes: {
-				line: {
-					from: { transform: 'scale(0)' },
-					to: { transform: 'scale(1)' }
-				},
-				title: {
-					'0%': { opacity: 0 },
-					'85%': { opacity: .15 },
-					to: {
-						opacity: 0,
-						left: '-20%'
-					}
-				}
-			}
-		}
+		extend: {}
 	},
 	plugins: [require('tailwindcss-animated')]
 };
